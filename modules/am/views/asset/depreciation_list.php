a0aeea9b53507fffda2fa57b8a4ddc3c5bab94b7
patjawat <patjawat@gmail.com>
2024-04-23 23:57:34 +0700

    update report

--- modules/am/views/asset/depreciation_list.php
+++ modules/am/views/asset/depreciation_list.php
@@ -272,7 +272,6 @@ use yii\helpers\Html;
               </span>
             </td>
             <td class="text-center">
-                <?php // Html::a('<i class="fa-solid fa-print"></i>',['/ms-word/asset','id' => $model->id,'number' => $data['date_number']], ['target'=>'_blank'])?>
                 <?=Html::a('<i class="fa-solid fa-print"></i>',['/ms-word/asset','id' => $model->id,'number' => $data['date_number'],'date' => $data['date']], ['class'=> 'open-modal','data' => ['size' => 'modal-xl']])?>
             </td>
         </tr>
