4ff408e2007dba72d75658444cfa93dbb24d6c47
patjawat <patjawat@gmail.com>
2024-05-05 20:37:14 +0700

    update qrcode

--- modules/am/views/asset/qr_code.php
+++ modules/am/views/asset/qr_code.php
@@ -1,13 +1,35 @@
 <?php
+use yii\helpers\Html;
 use chillerlan\QRCode\QRCode;
  $data = 'https://programmerthailand.com';
  $qr = new QRCode();
 ?>
 <div class="d-flex align-items-center">
     <div class="flex-shrink-0">
-      <img src="<?=$qr->render($data)?>" alt="...">
-  </div>
-  <div class="flex-grow-1 ms-3">
-    This is some content from a media component. You can replace this with any content and adjust it as needed.
-  </div>
+        <img src="<?=$qr->render($data)?>" width="140">
+    </div>
+    <div class="flex-grow-1 ms-3">
+        <ul class="list-inline">
+            <li><i class="bi bi-check2-circle text-primary fs-5"></i>
+                <span class="text-danger"><?=$model->code?><span>
+            </li>
+            <li><i class="bi bi-check2-circle text-primary fs-5"></i>
+                <?=isset($model->data_json['asset_type_text']) ? $model->data_json['asset_type_text'] : '-'?>
+            <li><i class="bi bi-check2-circle text-primary fs-5"></i>
+                <?=number_format($model->price, 2)?>
+                :: <?=Yii::$app->thaiFormatter->asDate($model->receive_date, 'short')?>
+            </li>
+            <li><i class="bi bi-check2-circle text-primary fs-5"></i>
+                <?php if (isset($model->data_json['department_name']) && $model->data_json['department_name'] == ''): ?>
+                <?=isset($model->data_json['department_name_old']) ? $model->data_json['department_name_old'] : ''?>
+                <?php else: ?>
+                <?=isset($model->data_json['department_name']) ? $model->data_json['department_name'] : ''?>
+                <?php endif;?>
+            </li>
+            <ul>
+    </div>
+</div>
+<div class="flex gap-2">
+    <?=Html::a('<i class="fa-solid fa-print"></i> QR-Code',['#'],['class' => 'btn btn-primary'])?>
+    <?=Html::a('<i class="fa-solid fa-print"></i> Barcode',['#'],['class' => 'btn btn-primary'])?>
 </div>
