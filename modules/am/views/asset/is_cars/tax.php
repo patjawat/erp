7bc1ca983ad59ddd6010c6be7f7a9f09ab6275d3
patjawat <patjawat@gmail.com>
2024-05-06 21:28:49 +0700

    update design

--- modules/am/views/asset/is_cars/tax.php
+++ modules/am/views/asset/is_cars/tax.php
@@ -3,7 +3,6 @@ use yii\helpers\Html;
 use app\modules\am\models\AssetDetail;
 
 $modelDetail = AssetDetail::find()->where(['name' => "tax_car",'code'=>$model->code])->orderBy(['date_start' => SORT_DESC])->one();
-
 ?>
 
 <div class="alert alert-success" role="alert">
@@ -11,26 +10,20 @@ $modelDetail = AssetDetail::find()->where(['name' => "tax_car",'code'=>$model->c
         <span class="fw-semibold">
             <i class="fa-solid fa-car-on fs-4"></i> ข้อมูลการต่อภาษี
         </span>
-
     </div>
-    <?php if(isset($modelDetail->data_json['company1']) && $modelDetail->data_json['company1'] != ""):?>
-        <hr>
     <strong><i class="fa-solid fa-user-injured"></i> พรบ.</strong>
-    <div class="border-bottom">
-        <div class="row">
-            <div class="col-6">
-                <ul class="list-inline">
-                    <li><i class="bi bi-check2-circle text-primary fs-5"></i> <span class="fw-semibold">บริษัท</span> :
-                    <?=isset($modelDetail->data_json['company1']) ? $modelDetail->data_json['company1'] : '-'?></li>
-                    <li><i class="bi bi-check2-circle text-primary fs-5"></i> <span
-                    class="fw-semibold">กรมธรรม์เลขที่</span>:
-                    <?=isset($modelDetail->data_json['number1']) ? $modelDetail->data_json['number1'] : '-'?></li>
-                </ul>
-            </div>
-            
-            <div class="col-6">
-            <ul class="list-inline">
-                <li><i class="bi bi-check2-circle text-primary fs-5"></i> <span class="fw-semibold">เริ่มต้น</span>
+    <table class="table border-0 table-striped-columns">
+        <tbody>
+            <?php if(isset($modelDetail->data_json['company1']) && $modelDetail->data_json['company1'] != ""):?>
+            <tr class="">
+                <td class="text-end"><span class="fw-semibold">บริษัท :</span></td>
+                <td> <?=isset($modelDetail->data_json['company1']) ? $modelDetail->data_json['company1'] : '-'?></td>
+                <td class="text-end"><span class="fw-semibold">กรมธรรม์เลขที่ :</span></td>
+                <td><?=isset($modelDetail->data_json['number1']) ? $modelDetail->data_json['number1'] : '-'?></td>
+            </tr>
+            <tr>
+                <td class="text-end"><span class="fw-semibold">เริ่มต้น :</span></td>
+                <td>
                     <?php
                     try {
                         echo isset($modelDetail->data_json["date_start1"]) ? Yii::$app->thaiFormatter->asDate($modelDetail->data_json["date_start1"], 'medium') : '';
@@ -38,38 +31,33 @@ $modelDetail = AssetDetail::find()->where(['name' => "tax_car",'code'=>$model->c
                        echo '-';
                     }
                     ?>
-                </li>
-                <li><i class="bi bi-check2-circle text-primary fs-5"></i> <span class="fw-semibold">สิ้นสุด</span>
-                <?php
+                </td>
+                <td class="text-end"><span class="fw-semibold">ถึง :</span></td>
+                <td> <?php
                     try {
                         echo isset($modelDetail->data_json["date_end1"]) ? Yii::$app->thaiFormatter->asDate($modelDetail->data_json["date_end1"], 'medium') : '';
                     } catch (\Throwable $th) {
                        echo '-';
                     }
-                    ?>
-                </li>
-            </ul>
-    </div>
-</div>
-</div>
-<?php endif;?>
-<?php if(isset($modelDetail->data_json['company2']) && $modelDetail->data_json['company2'] != ""):?>
+                    ?></td>
+            </tr>
+            <?php endif;?>
+        </tbody>
+    </table>
 
-        <strong><i class="fa-solid fa-car-burst"></i> ประกันภัย</strong>
-    <div class="row">
-        <div class="col-6">
-            <ul class="list-inline">
-                <li><i class="bi bi-check2-circle text-primary fs-5"></i> <span class="fw-semibold">บริษัท</span> :
-                    <?= isset($modelDetail->data_json['company2']) ? $modelDetail->data_json['company2'] : '-'?></li>
-                <li><i class="bi bi-check2-circle text-primary fs-5"></i> <span
-                        class="fw-semibold">กรมธรรม์เลขที่</span>:
-                    <?=isset($modelDetail->data_json['number2']) ? $modelDetail->data_json['number2'] : '-'?></li>
-            </ul>
-        </div>
-
-        <div class="col-6">
-            <ul class="list-inline">
-                <li><i class="bi bi-check2-circle text-primary fs-5"></i> <span class="fw-semibold">เริ่มต้น</span>
+    <strong><i class="fa-solid fa-car-burst"></i> ประกันภัย</strong>
+    <table class="table border-0 table-striped-columns">
+        <tbody>
+            <?php if(isset($modelDetail->data_json['company2']) && $modelDetail->data_json['company2'] != ""):?>
+            <tr class="">
+                <td class="text-end"><span class="fw-semibold">บริษัท :</span></td>
+                <td> <?=isset($modelDetail->data_json['company2']) ? $modelDetail->data_json['company2'] : '-'?></td>
+                <td class="text-end"><span class="fw-semibold">กรมธรรม์เลขที่ :</span></td>
+                <td><?=isset($modelDetail->data_json['number2']) ? $modelDetail->data_json['number2'] : '-'?></td>
+            </tr>
+            <tr>
+                <td class="text-end"><span class="fw-semibold">เริ่มต้น :</span></td>
+                <td>
                     <?php
                     try {
                         echo isset($modelDetail->data_json["date_start2"]) ? Yii::$app->thaiFormatter->asDate($modelDetail->data_json["date_start2"], 'medium') : '';
@@ -77,22 +65,21 @@ $modelDetail = AssetDetail::find()->where(['name' => "tax_car",'code'=>$model->c
                        echo '-';
                     }
                     ?>
-                </li>
-                <li><i class="bi bi-check2-circle text-primary fs-5"></i> <span class="fw-semibold">สิ้นสุด</span>
-                <?php
+                </td>
+                <td class="text-end"><span class="fw-semibold">ถึง :</span></td>
+                <td> <?php
                     try {
                         echo isset($modelDetail->data_json["date_end2"]) ? Yii::$app->thaiFormatter->asDate($modelDetail->data_json["date_end2"], 'medium') : '';
                     } catch (\Throwable $th) {
                        echo '-';
                     }
-                    ?>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <?php endif;?>
-    <hr class="mt-1">
+                    ?></td>
+            </tr>
+            <?php endif;?>
+        </tbody>
+    </table>
 
+    <hr class="mt-1">
     <div class="d-flex justify-content-between align-items-center">
         <ul class="list-inline mb-0">
             <li><i class="fa-regular fa-calendar-check fs-5"></i> <span class="">วันที่ต่อภาษี</span> :
@@ -105,11 +92,7 @@ $modelDetail = AssetDetail::find()->where(['name' => "tax_car",'code'=>$model->c
                     <?= isset($modelDetail->date_end) ? Yii::$app->thaiFormatter->asDate($modelDetail->date_end, 'medium') : '-' ?>
                 </span>
             </li>
-
         </ul>
         <?=Html::a('<i class="fa-solid fa-gear"></i> ดำเนินการ',['/am/asset-detail/','id'=> $model->id,'name' => 'tax_car','title' => '<i class="fa-solid fa-car-on"></i> ข้อมูลการต่อภาษี',"code"=>$model->code],['class' => 'btn btn-primary rounded-pill border border-white open-modal','data' => ['size' => 'modal-xl']])?>
-
     </div>
-
-
 </div>
