309b3744cd87f2e5560abd3d22e7114802a33e6b
patjawat <patjawat@gmail.com>
2024-05-14 18:59:14 +0700

    update

--- themes/v3/layouts/theme-v/header.php
+++ themes/v3/layouts/theme-v/header.php
@@ -71,90 +71,9 @@ use app\components\UserHelper;
                 </div>
             </div>
 
-
-            <div class="d-none d-lg-inline-flex ms-2 dropdown" data-aos="zoom-in" data-aos-delay="100">
-                <button data-bs-toggle="dropdown" aria-haspopup="true" type="button" id="page-header-app-dropdown"
-                    aria-expanded="false" class="btn header-item notify-icon">
-                    <i class="bx bx-customize"></i>
-                </button>
-                <div aria-labelledby="page-header-app-dropdown"
-                    class="dropdown-menu-lg dropdown-menu-right dropdown-menu" style="width: 600px;">
-                    <div class="px-lg-2">
-
-                        <h6 class="text-center mt-3"><i class="fa-solid fa-grip"></i> บริการ</h6>
-
-                        <!-- App Service -->
-                        <div class="row p-3">
-                        <div class="col-4 mt-1">
-                                <a href="<?=Url::to(['/helpdesk/default/repair-select','title' => '<i class="fa-regular fa-square-check"></i> เลือกประเภทการซ่อม']);?>"
-                                    class="open-modal" data-title="xxx">
-                                    <div
-                                        class="d-flex flex-column align-items-center justify-content-center bg-light p-3 rounded-2">
-                                        <i class="fa-solid fa-triangle-exclamation fs-3"></i>
-                                        <div>แจ้งซ่อม</div>
-                                    </div>
-                                </a>
-                            </div>
-
-                            <!-- <div class="col-4">
-                                <div
-                                    class="d-flex flex-column align-items-center justify-content-center bg-light p-3 rounded-2">
-                                    <?=html::img('@web/images/svg-icons/leave.svg',['width' => '50px'])?>
-                                    <div>ระบบลา</div>
-                                </div>
-                            </div>
-                            <div class="col-4">
-                                <div
-                                    class="d-flex flex-column align-items-center justify-content-center bg-light p-3 rounded-2">
-                                    <?=html::img('@web/images/svg-icons/booking.svg',['width' => '50px'])?>
-                                    <div>ระบบจองรถ</div>
-                                </div>
-                            </div>
-
-                            <div class="col-4">
-                                <div
-                                    class="d-flex flex-column align-items-center justify-content-center bg-light p-3 rounded-2">
-                                    <?=html::img('@web/images/svg-icons/meeting.svg',['width' => '50px'])?>
-                                    <div>ระบบจองห้องประชุม</div>
-                                </div>
-                            </div>
-
-                            <div class="col-4 mt-3">
-                                <div
-                                    class="d-flex flex-column align-items-center justify-content-center bg-light p-3 rounded-2">
-                                    <?=html::img('@web/images/svg-icons/document.svg',['width' => '50px'])?>
-                                    <div>ระบบสารบัญ</div>
-                                </div>
-                            </div>
-                            <div class="col-4 mt-3">
-                                <div
-                                    class="d-flex flex-column align-items-center justify-content-center bg-light p-3 rounded-2">
-                                    <?=html::img('@web/images/svg-icons/check-list.svg',['width' => '50px'])?>
-                                    <div>ระบบความเสี่ยง</div>
-                                </div>
-                            </div> -->
-                           
-
-                        </div>
-
-
-                        <h6 class="text-center mt-3"><i class="fa-solid fa-grip"></i> ระบบงาน</h6>
-                        <div class="row p-3">
-                        <div class="col-4 mt-1">
-                                <a href="<?=Url::to(['/helpdesk/default']);?>">
-                                    <div
-                                        class="d-flex flex-column align-items-center justify-content-center bg-light p-3 rounded-2">
-                                        <i class="fa-solid fa-screwdriver-wrench fs-3"></i>
-                                        <div>ระบบงานซ่อม</div>
-                                    </div>
-                                </a>
-                            </div>
-                            </div>
-
-
-                    </div>
-                </div>
-            </div>
+<?=$this->render('app_service')?>
+<?=$this->render('app_manage')?>
+            
             <div class="d-none d-lg-inline-flex ms-2" data-aos="zoom-in" data-aos-delay="200">
                 <button type="button" data-bs-toggle="fullscreen" class="btn header-item notify-icon" id="full-screen">
                     <i class="bx bx-fullscreen"></i>
