2b715ab2ccf68882f5ed62afc5165b0ee5d4c8cb
patjawat <patjawat@gmail.com>
2024-05-15 19:31:55 +0700

    update

--- modules/helpdesk/controllers/GeneralController.php
+++ modules/helpdesk/controllers/GeneralController.php
@@ -2,11 +2,38 @@
 
 namespace app\modules\helpdesk\controllers;
 
+use Yii;
+use app\modules\helpdesk\models\Helpdesk;
+use app\modules\helpdesk\models\HelpdeskSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\web\Response;
+
 class GeneralController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $searchModel = new HelpdeskSearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+        $dataProvider->query->andFilterWhere(['name' => 'repair']);
+        $dataProvider->query->andFilterWhere(['in', new \yii\db\Expression("JSON_EXTRACT(data_json, '$.repair_status')"), ["ร้องขอ"]]);
+
+        if($this->request->isAjax){
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            return [
+                'title' => '',
+                'content' => $this->renderAjax('index', [
+                    'searchModel' => $searchModel,
+                    'dataProvider' => $dataProvider,
+                ])
+            ];
+        }else{
+            return $this->render('index', [
+                'searchModel' => $searchModel,
+                'dataProvider' => $dataProvider,
+            ]);
+        }
     }
 
 }
--- modules/helpdesk/views/general/index.php
+++ modules/helpdesk/views/general/index.php
@@ -115,6 +115,7 @@ $this->title = "งานซ่อมบำรุง";
                 </div>
                 <table class="table  m-b-0 transcations mt-2">
                     <tbody>
+                    <?php foreach ($dataProvider->getModels() as $model): ?>
                         <tr>
                             <td style="width:20px;">
                                 <div class="main-img-user avatar-md">
@@ -124,7 +125,7 @@ $this->title = "งานซ่อมบำรุง";
                             <td>
                                 <div class="d-flex align-middle ms-3">
                                     <div class="d-inline-block">
-                                        <h6 class="mb-1">เครื่องคอมพิวเตอร์ขัดข้อง</h6>
+                                        <?=Html::a($model->data_json['title'],['/helpdesk/repair/view','id' => $model->id,'title' => '<i class="fa-solid fa-circle-exclamation text-danger"></i> แจ้งซ่อม'],['class' => 'h6 mb-1'])?>
                                         <p class="mb-0 fs-13 text-muted">OPD1</p>
                                     </div>
                                 </div>
@@ -136,29 +137,7 @@ $this->title = "งานซ่อมบำรุง";
                                 </div>
                             </td>
                         </tr>
-                        <tr>
-                            <td>
-                                <div class="main-img-user avatar-md">
-                                    <?=Html::img('@web/img/patjwat2.png',['class' => 'avatar avatar-md bg-primary text-white'])?>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="d-flex align-middle ms-3">
-                                    <div class="d-inline-block">
-                                        <h6 class="mb-1">น้ำไม่ไหล</h6>
-                                        <p class="mb-0 fs-13 text-muted">IPD2</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="text-end">
-                                <div class="d-inline-block">
-                                    <h6 class="mb-2 fs-15 fw-semibold">ด่วนที่สุด</h6>
-                                    <p class="mb-0 fs-11 text-muted">23 ม.ค. 2567</p>
-                                </div>
-                            </td>
-                        </tr>
-
-
+                    <?php endforeach?>
                     </tbody>
                 </table>
 
