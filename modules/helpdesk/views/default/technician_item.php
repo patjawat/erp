e90ae8ffd3b7c6283905ebabbaf7072fdc842c21
patjawat <patjawat@gmail.com>
2024-05-18 20:35:52 +0700

    text-bg-light

--- modules/helpdesk/views/default/technician_item.php
+++ modules/helpdesk/views/default/technician_item.php
@@ -22,7 +22,7 @@ use app\modules\hr\models\Employees;
 ?>
 <?php foreach($querys as $model):?>
 
-<div class="d-flex flex-column total font-weight-bold mt-1 bg-secondary-subtle rounded p-3 gap-2">
+<div class="d-flex flex-column total font-weight-bold mt-1 text-bg-light rounded p-3 gap-2">
     <?php 
          $employee = Employees::find()->where(['user_id' => $model['user_id']])->one();
          if($employee){
