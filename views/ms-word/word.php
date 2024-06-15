799945b4790a853d74cd255502f3732871edad5f
patjawat <patjawat@gmail.com>
2024-06-15 23:42:10 +0700

    update

--- views/ms-word/word.php
+++ views/ms-word/word.php
@@ -6,9 +6,9 @@ use yii\web\View;
 
 
 <?php // Html::a('<i class="fa-solid fa-cloud-arrow-down"></i> ดาวน์โหลดเอกสาร', Url::to(Yii::getAlias('@web') . '/msword/results/'.$filename.'.docx'), ['class' => 'btn btn-primary text-center mb-3','target' => '_blank','onclick' =>'return closeModal()']) ?>
-<!-- <iframe src='https://view.officeapps.live.com/op/embed.aspx?src=<?= Url::to(Yii::getAlias('@web') . '/msword/results/' . $filename, true); ?>' width='100%' height='1000px' frameborder='0'>
-</iframe> -->
-    <iframe src="https://docs.google.com/gview?url=<?= Url::to(Yii::getAlias('@web') . '/msword/results/' . $filename, true); ?>&embedded=true" width='100%' height='1000px frameborder="0"></iframe>
+<iframe src='https://view.officeapps.live.com/op/embed.aspx?src=<?= Url::to(Yii::getAlias('@web') . '/msword/results/' . $filename, true); ?>' width='100%' height='1000px' frameborder='0'>
+</iframe>
+    <!-- <iframe src="https://docs.google.com/gview?url=<?= Url::to(Yii::getAlias('@web') . '/msword/results/' . $filename, true); ?>&embedded=true" width='100%' height='1000px frameborder="0"></iframe> -->
 
 <?php
 $js = <<< JS
