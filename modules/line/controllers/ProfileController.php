72cdcdadfd8a2d12660830fc15e9fe406198afb5
patjawat <patjawat@gmail.com>
2024-07-22 17:45:44 +0700

    update

--- modules/line/controllers/ProfileController.php
+++ modules/line/controllers/ProfileController.php
@@ -6,10 +6,26 @@ use app\modules\hr\models\Employees;
 
 class ProfileController extends \yii\web\Controller
 {
+
+    public function beforeAction($action) {
+     
+        // $visit_ = TCDSHelper::getVisit();
+        // $hn = $visit_['hn'];
+        if (Yii::$app->user->isGuest) {
+            return $this->redirect(['/line/register']);
+        }
+        // if(empty($vn)){
+        //     return $this->redirect(['/nursescreen']);
+        // }
+        // return parent::beforeAction($action);
+    }
+
+
     public function actionIndex()
     {
 
         $model = Employees::findOne(8);
+
         return $this->render('index',[
             'model' => $model
         ]);
