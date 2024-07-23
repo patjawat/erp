ab6766f56dccc2168759115b52f94befde691223
patjawat <patjawat@gmail.com>
2024-07-23 21:10:52 +0700

    update

--- modules/line/views/profile/index.php
+++ modules/line/views/profile/index.php
@@ -16,51 +16,103 @@ use yii\helpers\Html;
     </div>
 </div>
 
+
+
 <?php
-use yii\helpers\Url;
+use app\components\SiteHelper;
+
 $urlCheckProfile = Url::to(['/line/auth/check-profile']);
-$liffProfile = SiteHelper::getInfo()['line_liff_profile'];
+$liffApp = SiteHelper::getInfo()['line_liff_app'];
 $liffRegisterUrl = 'https://liff.line.me/'.SiteHelper::getInfo()['line_liff_register'];
 
 $js = <<< JS
 
-      async function checkProfile(){
-          const {userId} = await liff.getProfile()
-          await $.ajax({
-              type: "post",
-              url: "$urlCheckProfile",
-              data:{
-                  line_id:userId
-              },
-              dataType: "json",
-              success: function (res) {
-                  console.log(res);
-                  if(res.status == false){
-                      location.replace("$liffRegisterUrl");
-                  }
-                  if(res.status == true){
-                      $('#avatar').html(res.avatar)
-                      $('#loading').hide()
-                  }
-              }
-          });
-          console.log('check profile');
-      }
-
-    function runApp() {
-          liff.getProfile().then(profile => {
-            checkProfile()
-          }).catch(err => console.error(err));
+async function checkProfile(){
+    const {userId} = await liff.getProfile()
+    await $.ajax({
+        type: "post",
+        url: "$urlCheckProfile",
+        data:{
+            line_id:userId
+        },
+        dataType: "json",
+        success: function (res) {
+            console.log(res);
+            if(res.status == false){
+                location.replace("$liffRegisterUrl");
+            }
+            if(res.status == true){
+                $('#avatar').html(res.avatar)
+                $('#loading').hide()
+            }
         }
-        
-        liff.init({ liffId: "$liffProfile"}, () => {
-          if (liff.isLoggedIn()) {
-            runApp()
-          } else {
-            liff.login();
-          }
-        }, err => console.error(err.code, error.message));
+    });
+    console.log('check profile');
+}
+
+function runApp() {
+      liff.getProfile().then(profile => {
+        checkProfile()
+      }).catch(err => console.error(err));
+    }
+    liff.init({ liffId: "$liffApp"}, () => {
+      if (liff.isLoggedIn()) {
+        runApp()
+      } else {
+        liff.login();
+      }
+    }, err => console.error(err.code, error.message));
 
 JS;
 $this->registerJs($js,View::POS_END);
+?>
+
+
+<?php
+// use yii\helpers\Url;
+// $urlCheckProfile = Url::to(['/line/auth/check-profile']);
+// $liffProfile = SiteHelper::getInfo()['line_liff_profile'];
+// $liffRegisterUrl = 'https://liff.line.me/'.SiteHelper::getInfo()['line_liff_register'];
+
+// $js = <<< JS
+
+//       async function checkProfile(){
+//           const {userId} = await liff.getProfile()
+//           await $.ajax({
+//               type: "post",
+//               url: "$urlCheckProfile",
+//               data:{
+//                   line_id:userId
+//               },
+//               dataType: "json",
+//               success: function (res) {
+//                   console.log(res);
+//                   if(res.status == false){
+//                       location.replace("$liffRegisterUrl");
+//                   }
+//                   if(res.status == true){
+//                       $('#avatar').html(res.avatar)
+//                       $('#loading').hide()
+//                   }
+//               }
+//           });
+//           console.log('check profile');
+//       }
+
+//     function runApp() {
+//           liff.getProfile().then(profile => {
+//             checkProfile()
+//           }).catch(err => console.error(err));
+//         }
+        
+//         liff.init({ liffId: "$liffProfile"}, () => {
+//           if (liff.isLoggedIn()) {
+//             runApp()
+//           } else {
+//             liff.login();
+//           }
+//         }, err => console.error(err.code, error.message));
+
+// JS;
+// $this->registerJs($js,View::POS_END);
 ?>
