b8cc534f6d6dcf1fafe1c97c7be62cf353b073ec
patjawat <patjawat@gmail.com>
2024-08-09 16:00:24 +0700

    update

--- modules/me/views/purchase/index.php
+++ modules/me/views/purchase/index.php
@@ -2,6 +2,8 @@
 use yii\helpers\Html;
 /** @var yii\web\View $this */
 ?>
+
+
         <div class="card">
             <div class="card-body">
                 <?= Html::a('<i class="fa-solid fa-bag-shopping"></i> ขอซื้อ/ขอจ้าง ', ['/purchase/pr-order/create', 'name' => 'order', 'title' => '<i class="bi bi-plus-circle"></i> เพิ่มใบขอซื้อ-ขอจ้าง'], ['class' => 'btn btn-primary rounded-pill shadow open-modal', 'data' => ['size' => 'modal-md']]) ?>
--- modules/me/views/store/index.php
+++ modules/me/views/store/index.php
@@ -1,7 +1,17 @@
 <?php
 use yii\helpers\Html;
 /** @var yii\web\View $this */
+$this->title = "เบิกวัสดุ/อุปกรณ์";
 ?>
+<?php $this->beginBlock('page-title'); ?>
+<i class="fa-solid fa-cart-plus"></i> <?= $this->title; ?>
+<?php $this->endBlock(); ?>
+<?php $this->beginBlock('sub-title'); ?>
+<?php $this->endBlock(); ?>
+
+
+
+
         <div class="card">
             <div class="card-body">
                 <?= Html::a('<i class="fa-solid fa-cart-plus"></i> เบิกวัสดุ ', ['/helpdesk/default/repair-select','title' => '<i class="fa-regular fa-circle-check"></i> เลือกประเภทการซ่อม'], ['class' => 'btn btn-primary rounded-pill shadow open-modal', 'data' => ['size' => 'modal-md']]) ?>
