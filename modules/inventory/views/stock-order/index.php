be9b5055d8b48622c6d71eee795cbc175c9fa58a
patjawat <patjawat@gmail.com>
2024-09-04 11:18:29 +0700

    update

--- modules/inventory/views/stock-order/index.php
+++ modules/inventory/views/stock-order/index.php
@@ -44,8 +44,6 @@ $createIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" vi
 
   <div class="card">
     <div class="card-body">
-
-      <div class="table-responsive">
         <div class="d-flex justify-content-between">
           <h6><i class="bi bi-ui-checks"></i> ขอเบิกจำนวน <span class="badge rounded-pill text-bg-primary"> <?=$dataProvider->getTotalCount()?></span> รายการ</h6>
           <div>
@@ -89,7 +87,6 @@ $createIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" vi
             <?php endforeach; ?>
           </tbody>
         </table>
-      </div>
 
     </div>
   </div>
