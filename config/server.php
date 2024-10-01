84a97e93a695e746857903ad409da12af512d8c9
patjawat <patjawat@gmail.com>
2024-10-01 22:11:00 +0700

    update

---
+++ config/server.php
@@ -0,0 +1,15 @@
+// config/server.php
+<?php
+  return [
+   'host' => 'localhost',
+   'port' => 9501,
+   'mode' => SWOOLE_PROCESS,
+   'sockType' => SWOOLE_SOCK_TCP,
+   'app' => require __DIR__ . '/swoole.php', 
+   'options' => [ // options for swoole server
+       'pid_file' => __DIR__ . '/../runtime/swoole.pid',
+       'worker_num' => 2,
+       'daemonize' => 0,
+       'task_worker_num' => 2,
+   ]
+];
