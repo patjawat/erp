5dc3b7c26ce10f1bf8ed343cee9ccf395a9829b6
patjawat <patjawat@gmail.com>
2024-10-04 14:15:51 +0700

    update

--- modules/sm/views/default/budget_balanced.php
+++ modules/sm/views/default/budget_balanced.php
@@ -1,17 +1,22 @@
 <?php
-
+$sql = "SELECT FORMAT(IFNULL(sum(i.price * i.qty),0),2) as total FROM orders o 
+INNER JOIN orders as i ON i.category_id = o.id AND i.name = 'order_item'
+WHERE o.thai_year = :thai_year";
+$query = Yii::$app->db->createCommand($sql)
+->bindValue(':thai_year','2568')
+->queryScalar();
 ?>
 <div class="row">
     <div class="col-12">
         <div class="card">
             <div class="card-body">
                 <div class="d-flex justify-content-between">
-                    <p style="margin-bottom:0px;">เงินงบประมาณ</p>
+                    <p style="margin-bottom:0px;">รวมเป็นเงินทั้งสิ้น</p>
                     <i class="fa-solid fa-wallet fs-1 text-secondary"></i>
                 </div>
                 <div class="">
-                        <span class="h5 fw-semibold">$800000000.6k</span>
-                        <p class="fw-lighter">ใช้จ่ายไปแล้วประมาณ 25% ของงบประมาณประจำปี</p>
+                        <span class="h5 fw-semibold"><?=$query?> บาท</span>
+                        <!-- <p class="fw-lighter">ใช้จ่ายไปแล้วประมาณ 25% ของงบประมาณประจำปี</p> -->
                     </div>
                     <div id="orderBudget"></div>
 
@@ -51,7 +56,28 @@ $js = <<< JS
           },
           xaxis: {
             categories: res.categorise,
-          }
+          },
+          yaxis: { show: true,
+              tickAmount: 4,
+              // labels: {
+                // },
+                labels: {
+                  offsetX: -17,
+                formatter: function (value) {
+                  return value.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                }
+              },
+              title: {
+                // text: '\$ (thousands)'
+              }
+            },
+            tooltip: {
+              y: {
+                formatter: function (val) {
+                  return val.toFixed(2) + " บาท"
+                }
+              }
+            }
           };
 
                     var chart = new ApexCharts(document.querySelector("#orderBudget"), orderBudgetOption);
