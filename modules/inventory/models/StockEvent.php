e23c87e6d68cb60b8ed289b9ebeb6fda76b91dc1
patjawat <patjawat@gmail.com>
2024-10-28 19:56:21 +0700

    update

--- modules/inventory/models/StockEvent.php
+++ modules/inventory/models/StockEvent.php
@@ -517,7 +517,7 @@ class StockEvent extends Yii\db\ActiveRecord
                     'status' => $status,
                     'fullname' => isset($this->data_json['checker_name']) ? $this->data_json['checker_name'] : '',
                     'checker_date' => isset($this->data_json['checker_confirm_date']) ?   explode(' ',Yii::$app->thaiFormatter->asDateTime($this->data_json['checker_confirm_date'], 'php:d/m/Y H:i:s'))[0] : '',
-                    'avatar' => $this->getAvatar($this->checker, '<span class="fw-bolder">ผู้อนุมัติ</span> ' . $status . ' | <i class="bi bi-clock"></i> <span class="text-muted fs-13">' . $checkerTime . '</span>')['avatar'],
+                    'avatar' => $this->getAvatar($this->checker, '<span class="fw-bolder">'.$msg.'</span> ' . $status . ' | <i class="bi bi-clock"></i> <span class="text-muted fs-13">' . $checkerTime . '</span>')['avatar'],
                 ];
         } catch (\Throwable $th) {
             return
