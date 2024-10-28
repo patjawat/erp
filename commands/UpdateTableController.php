6a63783b5e7f8e11e2073e4ac0276bc9d291daa5
patjawat <patjawat@gmail.com>
2024-10-28 15:36:55 +0700

    update

--- commands/UpdateTableController.php
+++ commands/UpdateTableController.php
@@ -8,6 +8,7 @@
 
 namespace app\commands;
 
+use Yii;
 use yii\console\Controller;
 use yii\helpers\BaseConsole;
 
@@ -20,7 +21,7 @@ use yii\helpers\BaseConsole;
  *
  * @since 2.0
  */
-class UpdateAuthController extends Controller
+class UpdateTableController extends Controller
 {
     /**
      * This command echoes what you have entered as the message.
@@ -202,4 +203,27 @@ class UpdateAuthController extends Controller
             ['child' => 'purchase/po-order/index', 'parent' => 'purchase'],
         ];
     }
+
+    public function actionBudgetGroup()
+    {
+        $check = Yii::$app->db->createCommand("select * from categorise where name ='budget_group'")->queryAll();
+        if(count($check) == 0){
+            $sql = "INSERT INTO `categorise` (`code`, `name`, `title`, `active`) VALUES
+                ('BG1', 'budget_group', 'งบบุคลากร', 1),
+                ('BG2', 'budget_group', 'งบดำเนินงาน (ค่าตอนแทน)', 1),
+                ('BG3', 'budget_group', 'งบดำเนินงาน (ค่าใช้สอย)', 1),
+                ('BG4', 'budget_group', 'งบดำเนินงาน (ค่าวัสดุ)', 1),
+                ('BG5', 'budget_group', 'งบดำเนินงาน (ค่าสาธารณูปโภค)', 1),
+                ('BG6', 'budget_group', 'งบลงทุน (ค่าครุภัณฑ์)', 1),
+                ('BG7', 'budget_group', 'งบลงทุน (ค่าที่ดินและสิ่งก่อสร้าง)', 1),
+                ('BG8', 'budget_group', 'งบเงินอุดหนุน', 1),
+                ('BG9', 'budget_group', 'งบรายจ่ายอื่น', 1),
+                ('BG10', 'budget_group', 'งบค่าเสื่อม', 1);";
+            Yii::$app->db->createCommand($sql)->execute();
+            echo 'สำเร็จ';
+        }else{
+            echo 'มีข้อมูลแล้ว';
+        }
+    }
+
 }
---
+++ db/update-db.php
@@ -0,0 +1,15 @@
+#!/bin/bash
+docker exec -it dansai yii update-table/budget-group &&
+docker exec -it ubonrat yii update-table/budget-group &&
+docker exec -it nampong yii update-table/budget-group &&
+docker exec -it umphang  yii update-table/budget-group &&
+docker exec -it lomkao  yii update-table/budget-group &&
+docker exec -it pua  yii update-table/budget-group &&
+docker exec -it thatphanom  yii update-table/budget-group &&
+docker exec -it yaha  yii update-table/budget-group &&
+docker exec -it denchai  yii update-table/budget-group &&
+docker exec -it phakhao  yii update-table/budget-group &&
+docker exec -it thawangpha yii update-table/budget-group &&
+docker exec -it chombung yii update-table/budget-group &&
+docker exec -it erawan  yii update-table/budget-group &&
+docker exec -it pcrh  yii update-table/budget-group
