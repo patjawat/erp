6b0947a05aa9c7cc7200628a0b2ce04e6a27a868
patjawat <patjawat@gmail.com>
2024-11-02 13:41:48 +0700

    update

---
+++ modules/helpdesk/views/stock/_search.php
@@ -0,0 +1,73 @@
+<?php
+
+use yii\helpers\Html;
+use kartik\select2\Select2;
+use yii\widgets\ActiveForm;
+$cart = Yii::$app->cartSub;
+$warehouse = Yii::$app->session->get('warehouse');
+/** @var yii\web\View $this */
+/** @var app\modules\inventory\models\StockSearch $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="stock-search">
+
+    <?php $form = ActiveForm::begin([
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+    <div class="d-flex gap-3">
+        <?= $form->field($model, 'q')->label(false) ?>
+        <?php
+  
+  echo $form->field($model, 'asset_type')->widget(Select2::classname(), [
+      'data' => $model->ListProductType(),
+      'options' => ['placeholder' => 'เลือกประเภทวัสดุ',
+  ],
+      'pluginOptions' => [
+          'allowClear' => true,
+          'width' => '200px',
+        ],
+        'pluginEvents' => [
+            'select2:select' => "function(result) { 
+                $(this).submit()
+                }",
+                'select2:unselect' => "function(result) { 
+                    $(this).submit()
+                    }",
+            ],
+            ])->label(false);
+            ?>
+            <?php if(isset($warehouse) && $warehouse['warehouse_type'] == 'SUB'):?>
+             <?=Html::a('<button type="button" class="btn btn-primary">
+                    <i class="fa-solid fa-cart-plus"></i> ตะกร้า <span class="badge text-bg-danger" id="totalCount">'.$cart->getCount().'</span>
+                    </button>',['/inventory/sub-stock/show-cart'],['class' => 'brn btn-primary shadow open-modal','data' => ['size' => 'modal-xl']])?>
+       <?php  endif;?>
+       <?php //  Html::submitButton('<i class="fa-solid fa-magnifying-glass"></i> ค้นหา', ['class' => 'btn btn-light']) ?>
+        <!-- <div class="btn-group">
+            <?php Html::a('<i class="bi bi-clock"></i> ดำเนินการ', ['/purchase/order/view', 'id' => $model->id], ['class' => 'btn btn-light w-100']) ?>
+            <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split"
+                data-bs-toggle="dropdown" aria-expanded="false" data-bs-reference="parent"> <i class="bi bi-caret-down-fill"></i>
+            </button>
+            <ul class="dropdown-menu">
+                <li>
+                    <?php //  Html::a('พิมพ์สต๊อกการ์ด', ['/ms-word/stockcard', 'id' => $model->id], ['class' => 'dropdown-item open-modal', 'data' => ['size' => 'modal-xl']]) 
+                    ?>
+                    <?php // Html::a('<i class="fa-solid fa-print me-1"></i> พิมพ์', ['/sm/order/document', 'id' => $model->id, 'title' => '<i class="fa-solid fa-print"></i> พิมพ์เอกสารประกอบการจัดซื้อ'], ['class' => 'dropdown-item open-modal', 'data' => ['size' => 'modal-md']]) 
+                    ?>
+
+                </li>
+            </ul>
+        </div> -->
+
+
+        <div class="form-group">
+
+        </div>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
--- modules/helpdesk/views/stock/index.php
+++ modules/helpdesk/views/stock/index.php
@@ -1,37 +1,52 @@
 <?php
 use yii\helpers\Url;
-use yii\helpers\Html
+use yii\helpers\Html;
+use yii\widgets\Pjax;
+use yii\db\Expression;
+use app\modules\inventory\models\Warehouse;
+//ตรวจสอบว่าเป็นผู้ดูแลคลัง
+$userid = \Yii::$app->user->id;
+$getWarehouse = Warehouse::find()->andWhere(new Expression("JSON_CONTAINS(data_json->'$.officer','\"$userid\"')"))->one();
+if ($getWarehouse) {
+    $warehouse = $warehouse->id;
+} else {
+    $warehouse = 0;
+}
+
 ?>
 <div
-    class="table-responsive"
+class="table-responsive"
 >
-    <table
-        class="table table-primary"
-    >
-        <thead>
-            <tr>
-                <th scope="col">รายการ</th>
-                <th scope="col">จำนวน</th>
-                <th scope="col">ราคา</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="">
-                <td scope="row">R1C1</td>
-                <td>R1C2</td>
-                <td>R1C3</td>
-            </tr>
-            <tr class="">
-                <td scope="row">Item</td>
-                <td>Item</td>
-                <td>Item</td>
-            </tr>
-            
-        </tbody>
-    </table>
+<table
+class="table table-primary"
+>
+<thead>
+    <tr>
+        <th scope="col">รายการ</th>
+        <th scope="col">จำนวน</th>
+        <th scope="col">ราคา</th>
+    </tr>
+</thead>
+<tbody>
+    <tr class="">
+        <td scope="row">R1C1</td>
+        <td>R1C2</td>
+        <td>R1C3</td>
+    </tr>
+    <tr class="">
+        <td scope="row">Item</td>
+        <td>Item</td>
+        <td>Item</td>
+    </tr>
+    
+</tbody>
+</table>
 </div>
 
-<div class="d-flex flex-wrap overflow-scroll" style="height:500px">
+<?php Pjax::begin(['id' => 'inventory-container', 'enablePushState' => true, 'timeout' => 88888888]); ?>
+<?php echo $this->render('_search', ['model' => $searchModel]); ?>
+
+<div class="d-flex flex-wrap overflow-scroll" style="height:500px;background-color: #eceff3;">
             <?php foreach ($dataProvider->getModels() as $model): ?>
             <div class="p-2 col-2">
                 <div class="card position-relative rounded">
@@ -40,7 +55,7 @@ use yii\helpers\Html
                     </p>
                     <?php echo Html::img($model->product->ShowImg(), ['class' => 'card-top']); ?>
                     <div class="card-body w-100">
-                        <a href="<?=Url::to(['/inventory/main-stock/add-to-cart', 'id' => $model->getLotQty()['id']])?>" class="add-cart">
+                        <a href="<?=isset($model->getLotQty()['id']) ? Url::to(['/inventory/main-stock/add-to-cart', 'id' => $model->getLotQty()['id']]) : '#'?>" class="add-cart">
                         
                         <div class="d-flex justify-content-start align-items-center">
                             <?php if($model->SumQty() >= 1):?>
@@ -74,3 +89,5 @@ use yii\helpers\Html
             </div>
             <?php endforeach?>
         </div>
+
+<?php Pjax::end(); ?>
