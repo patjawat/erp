7be23db8df90e8fb3cc19861cd15260222041b2c
patjawat <patjawat@gmail.com>
2024-12-01 18:20:24 +0700

    update script

--- run-production/build.sh
+++ run-production/build.sh
@@ -1,1 +1,5 @@
-docker buildx build --platform linux/amd64  -t erp:v1 ../. --load && docker push patjawat/erp
+docker buildx build --platform linux/amd64  -t erp:v1 ../. --load && 
+docker push patjawat/erp &&
+docker builder prune -a
+
+
