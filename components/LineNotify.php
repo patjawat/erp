e192a4191e083d113c9795ed6119de59fe4640f7
patjawat <patjawat@gmail.com>
2025-01-25 21:37:17 +0700

    update

--- components/LineNotify.php
+++ components/LineNotify.php
@@ -213,11 +213,11 @@ class LineNotify extends Component
                  'type' => 'box',
                  'layout' => 'vertical',
                  'contents' => [
-                    // [
-                    //     "type"=> "icon",
-                    //     "size"=> "sm",
-                    //     "url"=> "https://developers-resource.landpress.line.me/fx/img/review_gold_star_28.png"
-                    // ],
+                    [
+                        "type"=> "icon",
+                        "size"=> "sm",
+                        "url"=> "https://developers-resource.landpress.line.me/fx/img/review_gold_star_28.png"
+                    ],
                      [
                          'type' => 'text',
                          'text' => $altText.'เรื่อง'.$model->document->topic,
