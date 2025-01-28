e591afcd59f1625de5e8f3566811db0f7a45a5bd
patjawat <patjawat@gmail.com>
2025-01-28 19:56:51 +0700

    update

--- modules/hr/views/leave/index.php
+++ modules/hr/views/leave/index.php
@@ -23,7 +23,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <?php $this->beginBlock('page-action'); ?>
 <?php echo $this->render('@app/modules/hr/views/leave/menu') ?>
 <?php $this->endBlock(); ?>
-<?php Pjax::begin(['id' => 'leave-container', 'timeout' => 500000]); ?>
+<?php // Pjax::begin(['id' => 'leave-container', 'timeout' => 500000]); ?>
 
 <!-- Start BxStatus -->
 <div class="row">
@@ -137,4 +137,4 @@ $js = <<< JS
     JS;
 $this->registerJs($js);
 ?>
-<?php Pjax::end(); ?>
+<?php // Pjax::end(); ?>
