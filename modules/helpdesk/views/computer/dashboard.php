9f92ea6dd7dc50b4949cdd1648f0d1aec23e240f
patjawat <patjawat@gmail.com>
2025-04-01 22:25:22 +0700

    update

--- modules/helpdesk/views/computer/dashboard.php
+++ modules/helpdesk/views/computer/dashboard.php
@@ -3,10 +3,13 @@ use yii\helpers\Html;
 $this->title = 'ศูนย์คอมพิวเตอร์';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
 <?php $this->beginBlock('page-title'); ?>
-<i class="fa-solid fa-computer fs-1"></i> <?= $this->title; ?>
+<i class="fa-solid fa-screwdriver-wrench fs-1"></i> <?= $this->title; ?>
 <?php $this->endBlock(); ?>
-<?php $this->beginBlock('sub-title'); ?>
+
+<?php $this->beginBlock('page-action'); ?>
+<?php echo $this->render('menu') ?>
 <?php $this->endBlock(); ?>
 
 <div class="card vh-100 d-flex justify-content-center align-items-center">
