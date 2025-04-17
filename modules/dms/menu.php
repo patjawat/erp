86d4ef3c662574f2213e036fc883012b65d43685
patjawat <patjawat@gmail.com>
2025-04-17 12:02:57 +0700

    update summary

--- modules/dms/menu.php
+++ modules/dms/menu.php
@@ -7,6 +7,8 @@ use app\modules\am\models\AssetItem;
 try {
  $countReceive = $model->CountType('receive') ?? 0;
 $countSend = $model->CountType('send') ?? 0;
+$countAppointment = $model->CountType('appointment') ?? 0;
+$countAnnounce = $model->CountType('announce') ?? 0;
 } catch (\Throwable $th) {
     $countReceive =  0;
     $countSend =  0;
@@ -18,7 +20,7 @@ $countSend = $model->CountType('send') ?? 0;
     <?php echo Html::a('<i class="fa-solid fa-chart-simple me-1"></i> Dashboard',['/dms/dashboard'],['class' => 'btn btn-light'])?>
         <?php echo  Html::a('<i class="fa-solid fa-inbox"></i> หนังสือรับ <span class="badge rounded-pill badge-soft-primary text-primary fs-13 fw-semibold">'.$countReceive.'</span>',['/dms/documents/receive'],['class' => 'btn btn-light'])?>
         <?php echo  Html::a('<i class="fa-solid fa-paper-plane"></i> หนังสือส่ง <span class="badge rounded-pill badge-soft-primary text-primary fs-13 fw-semibold">'.$countSend.'</span>',['/dms/documents/send'],['class' => 'btn btn-light'])?>
-        <?php echo  Html::a('<i class="fa-solid fa-flag"></i> คำสั่ง <span class="badge rounded-pill badge-soft-primary text-primary fs-13 fw-semibold">'.$countSend.'</span>',['/dms/documents/appointment'],['class' => 'btn btn-light'])?>
-        <?php echo  Html::a('<i class="fa-solid fa-bullhorn"></i> ประกาศ/นโยบาย <span class="badge rounded-pill badge-soft-primary text-primary fs-13 fw-semibold">'.$countSend.'</span>',['/dms/documents/announce'],['class' => 'btn btn-light'])?>
+        <?php echo  Html::a('<i class="fa-solid fa-flag"></i> คำสั่ง <span class="badge rounded-pill badge-soft-primary text-primary fs-13 fw-semibold">'.$countAppointment.'</span>',['/dms/documents/appointment'],['class' => 'btn btn-light'])?>
+        <?php echo  Html::a('<i class="fa-solid fa-bullhorn"></i> ประกาศ/นโยบาย <span class="badge rounded-pill badge-soft-primary text-primary fs-13 fw-semibold">'.$countAnnounce.'</span>',['/dms/documents/announce'],['class' => 'btn btn-light'])?>
 
 </div>
