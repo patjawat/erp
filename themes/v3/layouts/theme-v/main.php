b3cfdf11994942875958686abd5fc52de20ee90f
patjawat <patjawat@gmail.com>
2025-05-22 10:23:46 +0700

    update main

--- themes/v3/layouts/theme-v/main.php
+++ themes/v3/layouts/theme-v/main.php
@@ -207,15 +207,15 @@ $this->registerJS($js, View::POS_END);
 
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/682df6ff98a1b819123fbd6c/1irpopm6l';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/682df6ff98a1b819123fbd6c/1irpopm6l';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
 </script>
 <!--End of Tawk.to Script-->
 
