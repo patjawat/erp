c938cb2579f0f2815665bb0b01325322db5cbadf
patjawat <patjawat@gmail.com>
2025-06-05 18:41:53 +0700

    update

--- themes/v3/layouts/theme-h/main.php
+++ themes/v3/layouts/theme-h/main.php
@@ -29,7 +29,7 @@ $moduleId = Yii::$app->controller->module->id;
     <?php $this->registerCsrfMetaTags() ?>
 
     <title><?= Html::encode($this->title) ?></title>
-    <link rel="stylesheet" href="https://lettstartdesign.com/marvel/app/assets/css/styles.min.css" />
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <?php $this->head() ?>
 </head>
