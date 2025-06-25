ed60079f696900d25d46ad14a666a79730122066
patjawat <patjawat@gmail.com>
2025-06-25 17:23:26 +0700

    update

--- modules/hr/views/leave/calendar/leave-item.php
+++ modules/hr/views/leave/calendar/leave-item.php
@@ -1,9 +1,15 @@
     <?php
     use yii\helpers\Html;
+    $color = '';
+    if (isset($item->leaveType) && isset($item->leaveType->data_json['color'])) {
+        $color = $item->leaveType->data_json['color'];
+    }
+    $leaveTypeCode = isset($item->leaveType) && isset($item->leaveType->code) ? $item->leaveType->code : '';
+    $leaveTypeTitle = isset($item->leaveType) && isset($item->leaveType->title) ? $item->leaveType->title : '-';
     ?>
-    <div class="d-flex align-items-center p-1 border border-4 border-end-0 border-top-0 border-bottom-0 border-primary <?=$item->leaveType?->code?>" style="cursor: pointer; <?= $item->leaveType?->data_json['color'] ? ('background-color:' . $item->leaveType?->data_json['color']) : '' ?>">
+    <div class="d-flex align-items-center p-1 border border-4 border-end-0 border-top-0 border-bottom-0 border-primary <?=$leaveTypeCode?>" style="cursor: pointer; <?='background-color:' .$color?>">
         <?=Html::img($item->employee->ShowAvatar(),["class" => "leave-avatar",'alt' => $item->employee->fullname])?>
         <div>
-            <span><?=$item->employee->fullname?>  <?= $item->leaveType?->title ?? '-' ?></span>
+            <span><?=$item->employee->fullname?>  <?= $leaveTypeTitle ?></span>
         </div>
     </div>
