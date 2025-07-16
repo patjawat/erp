c3616f2b31ab1cab1217a1d28dc7ca90667425d6
patjawat <patjawat@gmail.com>
2025-07-16 13:51:42 +0700

    update

--- run-production/build.sh
+++ run-production/build.sh
@@ -1,9 +1,9 @@
 # build image แล้ว push ขึ้น docker hub
 docker buildx build --platform linux/amd64  -t erp:1.1 ../. --load &&
 # สร้าง tag
-docker tag erp:1.1 patjawat/erp:1.1 &&
+# docker tag erp:1.1 patjawat/erp:1.1 &&
 # สร้าง image ขึ้น docker hub
-docker push patjawat/erp:1.1 &&
+# docker push patjawat/erp:1.1 &&
 # ลบ image ที่เก่าออก
 docker image prune -f
 
