c2e8eaa22ae849a74cf6e7e8b52da35385dd8acc
patjawat <patjawat@gmail.com>
2025-08-01 17:47:07 +0700

    update

--- modules/booking/views/vehicle/menu.php
+++ modules/booking/views/vehicle/menu.php
@@ -4,18 +4,7 @@ use yii\helpers\Html;
 
 $layout = app\components\SiteHelper::getInfo()['layout'];
 $menus = [
-    [
-        'title' => 'ขอใช้รถพยาบาล',
-        'active' => 'ambulance',
-        'url' => ['/booking/vehicle/ambulance'],
-        'icon' => '<i class="fa-solid fa-truck-medical me-2"></i>'
-    ],
-    [
-        'title' => 'ทะเบียนจัดรถยนต์',
-        'active' => 'work',
-        'url' => ['/booking/vehicle/work'],
-        'icon' => '<i class="fa-solid fa-car-on me-2"></i>'
-    ],
+
     [
         'title' => 'ทะเบียนครุภัณฑ์',
         'active' => 'asset',
@@ -36,18 +25,32 @@ $menus = [
 <li class="nav-item">
     <?= Html::a('<i class="fa-solid fa-gauge-high me-1"></i>Dashboard', ['/booking/vehicle/dashboard'], ['class' => 'nav-link']) ?>
 </li>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle <?= (isset($active) && $active == 'official' ? 'active' : '') ?>" href="#"
-            id="topnav-dashboard" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="bi bi-ui-checks me-2"></i> ขอใช้รถทั่วไป
-            <i class="bx bx-chevron-down"></i>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="topnav-dashboard">
-            <?= Html::a('<i class="bi bi-ui-checks me-2"></i> ทะเบียนคำขอใช้รถ', ['/booking/vehicle'], ['class' => 'dropdown-item']) ?>
-            <?= Html::a('<i class="fa-regular fa-calendar me-2"></i> ปฏิทินขอใช้รถยนต์ ', ['/booking/vehicle/calendar'], ['class' => 'dropdown-item']) ?>
-            <?= Html::a('<i class="bi bi-ui-checks me-2"></i> ทะเบียนการจัดสรรรถ (พขร.)', ['/booking/vehicle/work'], ['class' => 'dropdown-item']) ?>
-        </div>
-    </li>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle <?= (isset($active) && $active == 'official' ? 'active' : '') ?>" href="#"
+        id="topnav-dashboard" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="bi bi-ui-checks me-2"></i> ขอใช้รถทั่วไป
+        <i class="bx bx-chevron-down"></i>
+    </a>
+    <div class="dropdown-menu" aria-labelledby="topnav-dashboard">
+        <?= Html::a('<i class="bi bi-ui-checks me-2"></i> ทะเบียนคำขอใช้รถ', ['/booking/vehicle'], ['class' => 'dropdown-item']) ?>
+        <?= Html::a('<i class="fa-regular fa-calendar me-2"></i> ปฏิทินขอใช้รถยนต์ ', ['/booking/vehicle/calendar'], ['class' => 'dropdown-item']) ?>
+        <?= Html::a('<i class="bi bi-ui-checks me-2"></i> ทะเบียนการจัดสรรรถ (พขร.)', ['/booking/vehicle/work'], ['class' => 'dropdown-item']) ?>
+    </div>
+</li>
+
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle <?= (isset($active) && $active == 'ambulance' ? 'active' : '') ?>" href="#"
+        id="topnav-dashboard" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="fa-solid fa-truck-medical me-2"></i> ขอใช้รถพยาบาล
+        <i class="bx bx-chevron-down"></i>
+    </a>
+    <div class="dropdown-menu" aria-labelledby="topnav-dashboard">
+        <?= Html::a('<i class="bi bi-ui-checks me-2"></i> ทะเบียนคำขอใช้รถ', ['/booking/vehicle/ambulance'], ['class' => 'dropdown-item']) ?>
+        <?= Html::a('<i class="fa-regular fa-calendar me-2"></i> ปฏิทินขอใช้รถยนต์ ', ['/booking/vehicle/calendar'], ['class' => 'dropdown-item']) ?>
+        <?= Html::a('<i class="bi bi-ui-checks me-2"></i> ทะเบียนการจัดสรรรถ (พขร.)', ['/booking/vehicle/work'], ['class' => 'dropdown-item']) ?>
+    </div>
+</li>
+
 
 <?php if ($layout == 'horizontal'): ?>
     <?php foreach ($menus as $menu): ?>
@@ -64,7 +67,6 @@ $menus = [
         <?= Html::a('<i class="fa-solid fa-gauge-high"></i> Dashboard', ['/booking/vehicle/dashboard'], ['class' => 'btn btn-light']) ?>
         <?= Html::a('<i class="fa-solid fa-car"></i> ทะเบียนใช้รถยนต์ทั่วไป', ['/booking/vehicle/index'], ['class' => 'btn btn-light']) ?>
         <?= Html::a('<i class="fa-solid fa-truck-medical"></i> ทะเบียนใช้รถพยาบาล', ['/booking/vehicle/ambulance'], ['class' => 'btn btn-light']) ?>
-        <?= Html::a('<i class="fa-solid fa-user-tag"></i> ทะเบียนจัดสรร', ['/booking/vehicle/work'], ['class' => 'btn btn-light']) ?>
         <?= Html::a('<i class="fa-solid fa-calendar"></i> ปฏิทิน', ['/booking/vehicle/calendar'], ['class' => 'btn btn-light']) ?>
     </div>
 <?php endif; ?>
