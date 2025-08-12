4be432cf9213be54874b61963b555fb1a97c1364
patjawat <patjawat@gmail.com>
2025-08-12 23:54:46 +0700

    update

--- modules/hr/views/development/member.php
+++ modules/hr/views/development/member.php
@@ -7,7 +7,7 @@ $me = UserHelper::GetEmployee();
         <div class="card-header bg-light p-2">
             <div class="d-flex align-items-center justify-content-between">
                 <strong><i class="fa-solid fa-users me-2"></i>คณะเดินทาง</strong>
-                <?=$me->id == $model->emp_id ? Html::a('<i class="fa-solid fa-circle-plus"></i> เพิ่ม',['/me/development-detail/create','name' => 'member','development_id' => $model->id,'title' => '<i class="fa-solid fa-users me-2"></i>คณะเดินทาง'],['class' => 'btn btn-sm btn-primary rounded-pill shadow open-modal','data' => ['size' => 'modal-md']]) : ''?>
+                <?=Html::a('<i class="fa-solid fa-circle-plus"></i> เพิ่ม',['/me/development-detail/create','name' => 'member','development_id' => $model->id,'title' => '<i class="fa-solid fa-users me-2"></i>คณะเดินทาง'],['class' => 'btn btn-sm btn-primary rounded-pill shadow open-modal','data' => ['size' => 'modal-md']])?>
             </div>
         </div>
         <div class="card-body">
