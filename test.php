c529823a719a8cc035c99b8e1c9d9c978c2b5f54
patjawat <patjawat@gmail.com>
2025-09-19 15:24:48 +0700

    update test

--- test.php
+++ test.php
@@ -1,69 +1,106 @@
- SELECT x4.*, (x4.days - x4.use_leave) AS total,
-                        (
-                            CASE 
-                                WHEN x4.accumulation = 0 THEN 10
-                                WHEN (x4.days - x4.use_leave + 10) > x4.max_days AND x4.accumulation = 1 THEN x4.max_days
-                                ELSE (x4.days - x4.use_leave + 10)
-                            END
-                        ) AS froward_days
-                    FROM (
-                        SELECT 
-                            x3.*,
-                            COALESCE(
-                                (SELECT days 
-                                FROM leave_entitlements 
-                                WHERE emp_id = x3.emp_id 
-                                AND leave_type_id = x3.leave_type_id 
-                                AND thai_year = 2568), 
-                                0
-                            ) AS days,
-                            COALESCE(
-                                (SELECT SUM(total_days) 
-                                FROM `leave` 
-                                WHERE emp_id = x3.emp_id 
-                                AND leave_type_id = x3.leave_type_id 
-                                AND thai_year = 2568), 
-                                0
-                            ) AS use_leave
-                        FROM (
+WITH stock_summary AS (
                             SELECT 
-                                x2.*,
-                                COALESCE((
-                                    SELECT max_days 
-                                    FROM `leave_policies` 
-                                    WHERE position_type_id = x2.position_type 
-                                    AND year_of_service <= x2.years_of_service 
-                                    ORDER BY year_of_service DESC 
-                                    LIMIT 1
-                                ),0) AS max_days
-                            FROM (
-                                SELECT 
-                                    x1.*
-                                FROM (
-                                    SELECT 
-                                        e.id AS emp_id,
-                                        CONCAT(e.fname, ' ', e.lname) AS fullname,
-                                        lt.title AS leave_type_name,
-                                        l.leave_type_id,
-                                        e.position_type,
-                                        pt.title AS position_type_name,
-                                        COALESCE(lp.accumulation,0) as accumulation,
-                                        TIMESTAMPDIFF(YEAR, e.join_date, CURDATE()) AS years_of_service
-                                    FROM employees e
-                                    LEFT JOIN leave_policies lp 
-                                        ON lp.position_type_id = e.position_type
-                                    LEFT JOIN `leave` l 
-                                        ON e.id = l.emp_id 
-                                    AND l.leave_type_id = 'LT4'
-                                    JOIN categorise lt 
-                                        ON l.leave_type_id = lt.code 
-                                    AND lt.name = 'leave_type'
-                                    JOIN categorise pt 
-                                        ON e.position_type = pt.code 
-                                    AND pt.name = 'position_type'
-                                    GROUP BY e.id
-                                    ORDER BY e.id ASC
-                                ) AS x1
-                            ) AS x2
-                        ) AS x3
-                    ) AS x4
+                                
+                                asset_type.code AS asset_type_code,
+                                asset_type.title AS asset_type_name,
+
+                                -- ยอดยกมา (ก่อนเดือน)
+                                SUM(CASE WHEN o.movement_date < '2025-08-01' AND o.transaction_type = 'IN'  
+                                        THEN i.qty * i.unit_price ELSE 0 END) 
+                                -
+                                SUM(CASE WHEN o.movement_date < '2025-08-01' AND o.transaction_type = 'OUT' 
+                                        THEN i.qty * i.unit_price ELSE 0 END) AS balance_before,
+
+                                -- รับเข้าระหว่างเดือน
+                                SUM(CASE WHEN o.movement_date BETWEEN '2025-08-01' AND '2025-08-31' 
+                                        AND o.transaction_type = 'IN' 
+                                        THEN i.qty * i.unit_price ELSE 0 END) AS total_in_month,
+
+                                -- จ่ายไประหว่างเดือน
+                                SUM(CASE WHEN o.movement_date BETWEEN '2025-08-01' AND '2025-08-31' 
+                                        AND o.transaction_type = 'OUT' 
+                                        THEN i.qty * i.unit_price ELSE 0 END) AS total_out_month
+
+                            FROM stock_events o
+                            LEFT JOIN stock_events i ON i.category_id = o.id
+                            LEFT JOIN categorise asset_type ON asset_type.code = o.asset_type_id
+                            LEFT JOIN warehouses w ON w.id = o.warehouse_id
+                            WHERE o.name = 'order'
+                             AND o.order_status = 'success'
+                            AND i.name = 'order_item'
+                            AND w.warehouse_type = 'MAIN'
+                            AND o.warehouse_id = 7
+                            AND asset_type.category_id = 4
+                            AND asset_type.name = 'asset_type'
+                            GROUP BY asset_type.code, asset_type.title
+                        )
+
+                        SELECT 
+                            asset_type_code,
+                            asset_type_name,
+                            balance_before,                     -- 1. ยอดยกมา
+                            total_in_month,                     -- 2. รับเข้าระหว่างเดือน
+                            (balance_before + total_in_month) AS total_before_out,   -- 3. รวม
+                            total_out_month,                    -- 4. จ่ายไประหว่างเดือน
+                            (balance_before + total_in_month - total_out_month) AS balance_after  -- 5. ยอดยกไป
+                        FROM stock_summary
+                        ORDER BY CAST(SUBSTRING(asset_type_code, 2) AS UNSIGNED)
+
+
+
+
+
+
+
+                        WITH stock_summary AS (
+                            SELECT 
+                                a.title  as asset_name,
+							    a.code AS asset_code,
+                                asset_type.code AS asset_type_code,
+                                asset_type.title AS asset_type_name,
+
+                                -- ยอดยกมา (ก่อนเดือน)
+                                SUM(CASE WHEN o.movement_date < '2025-08-01' AND o.transaction_type = 'IN'  
+                                        THEN i.qty * i.unit_price ELSE 0 END) 
+                                -
+                                SUM(CASE WHEN o.movement_date < '2025-08-01' AND o.transaction_type = 'OUT' 
+                                        THEN i.qty * i.unit_price ELSE 0 END) AS balance_before,
+
+                                -- รับเข้าระหว่างเดือน
+                                SUM(CASE WHEN o.movement_date BETWEEN '2025-08-01' AND '2025-08-31' 
+                                        AND o.transaction_type = 'IN' 
+                                        THEN i.qty * i.unit_price ELSE 0 END) AS total_in_month,
+
+                                -- จ่ายไประหว่างเดือน
+                                SUM(CASE WHEN o.movement_date BETWEEN '2025-08-01' AND '2025-08-31' 
+                                        AND o.transaction_type = 'OUT' 
+                                        THEN i.qty * i.unit_price ELSE 0 END) AS total_out_month
+
+                            FROM stock_events o
+                            LEFT JOIN stock_events i ON i.category_id = o.id
+    						LEFT JOIN categorise a ON a.code = i.asset_item
+                            LEFT JOIN categorise asset_type ON asset_type.code = o.asset_type_id
+                            LEFT JOIN warehouses w ON w.id = o.warehouse_id
+                            WHERE o.name = 'order'
+                             AND o.order_status = 'success'
+                            AND i.name = 'order_item'
+                            AND w.warehouse_type = 'MAIN'
+                            AND o.warehouse_id = 7
+                            AND asset_type.category_id = 4
+                            AND asset_type.name = 'asset_type'
+                            GROUP BY a.code
+                        )
+
+                        SELECT 
+                        asset_code,
+                        asset_name,
+                            asset_type_code,
+                            asset_type_name,
+                            balance_before,                     -- 1. ยอดยกมา
+                            total_in_month,                     -- 2. รับเข้าระหว่างเดือน
+                            (balance_before + total_in_month) AS total_before_out,   -- 3. รวม
+                            total_out_month,                    -- 4. จ่ายไประหว่างเดือน
+                            (balance_before + total_in_month - total_out_month) AS balance_after  -- 5. ยอดยกไป
+                        FROM stock_summary
+                        GROUP BY asset_code
+                        ORDER BY asset_code;
