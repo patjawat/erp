ab9969262cb21585f21afb92cfed1e005abc980d
patjawat <patjawat@gmail.com>
2025-12-21 11:35:16 +0700

    update title box

--- modules/dms/views/dashboard/index.php
+++ modules/dms/views/dashboard/index.php
@@ -5,7 +5,7 @@ $this->params['breadcrumbs'][] = $this->title;
 /** @var yii\web\View $this */
 ?>
 
-<?php $this->beginBlock('page-title'); ?>
+<!-- <?php $this->beginBlock('page-title'); ?>
 <div class="d-flex align-items-center gap-2 mb-2  text-primary-gradient">
     <h4 class="fw-medium text-body d-flex align-items-center gap-2 mb-0">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -15,6 +15,24 @@ $this->params['breadcrumbs'][] = $this->title;
             <rect width="7" height="5" x="3" y="16" rx="1"></rect>
         </svg>
         <?= $this->title; ?>
+    </h4>
+</div>
+<?php $this->endBlock(); ?> -->
+
+
+<?php $this->beginBlock('page-title'); ?>
+<div class="d-flex flex-column align-items-center align-items-lg-start gap-2 mb-2 text-primary-gradient text-center text-lg-start">
+    <h4 class="fw-medium text-body d-flex flex-column flex-sm-row align-items-center gap-2 mb-0">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-primary">
+            <rect width="7" height="9" x="3" y="3" rx="1"></rect>
+            <rect width="7" height="5" x="14" y="3" rx="1"></rect>
+            <rect width="7" height="9" x="14" y="12" rx="1"></rect>
+            <rect width="7" height="5" x="3" y="16" rx="1"></rect>
+        </svg>
+        
+        <span class="d-block">
+            <?= $this->title; ?>
+        </span>
     </h4>
 </div>
 <?php $this->endBlock(); ?>
