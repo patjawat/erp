01722d04093cc511e0825e1c8b65b2878b46681b
patjawat <patjawat@gmail.com>
2025-12-22 15:00:31 +0700

    แก้ไข action menu stock

--- modules/inventory/menu.php
+++ modules/inventory/menu.php
@@ -13,12 +13,21 @@ use yii\helpers\Url;
         </svg>
         ภาพรวม
     </a>
+    <a href="<?= Url::to(['/inventory/stock-in']) ?>" class="btn <?= $active !== 'stock-in' ? 'btn-outline-primary' : 'btn-primary' ?>">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-plus-corner-icon lucide-file-plus-corner">
+            <path d="M11.35 22H6a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.706.706l3.588 3.588A2.4 2.4 0 0 1 20 8v5.35" />
+            <path d="M14 2v5a1 1 0 0 0 1 1h5" />
+            <path d="M14 19h6" />
+            <path d="M17 16v6" />
+        </svg>
+        ทะเบียนรับเข้า
+    </a>
+
     <a href="<?= Url::to(['/inventory/warehouse/order-request']) ?>" class="btn <?= $active !== 'list' ? 'btn-outline-primary' : 'btn-primary' ?>">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path>
-            <path d="M16 3.128a4 4 0 0 1 0 7.744"></path>
-            <path d="M22 21v-2a4 4 0 0 0-3-3.87"></path>
-            <circle cx="9" cy="7" r="4"></circle>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-minus-icon lucide-file-minus">
+            <path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z" />
+            <path d="M14 2v5a1 1 0 0 0 1 1h5" />
+            <path d="M9 15h6" />
         </svg>
         ทะเบียนขอเบิกวัสดุ
     </a>
