98650545dcbaf143daeaa90dcd2e426947a97ed2
patjawat <patjawat@gmail.com>
2025-12-22 12:02:10 +0700

    update

--- themes/v4/layouts/navbar.php
+++ themes/v4/layouts/navbar.php
@@ -73,7 +73,7 @@ $menuItems = [
         'icon' => '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 3h20"></path><path d="M21 3v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V3"></path><path d="m7 21 5-5 5 5"></path></svg>'
     ],
     [
-         'show' => Yii::$app->user->can('development') ? true : false,
+         'show' => Yii::$app->user->can('hr') ? true : false,
         'label' => 'อบรม/ดูงาน', 
         'url' => ['/hr/development/dashboard'], 
         'active' => 'development',
