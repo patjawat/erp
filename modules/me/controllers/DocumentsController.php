17827c4ac928a45c1f7285ecac7d3208ddbb7f69
patjawat <patjawat@gmail.com>
2026-01-04 15:03:00 +0700

    แก้ไขการแสดงผล PDF

--- modules/me/controllers/DocumentsController.php
+++ modules/me/controllers/DocumentsController.php
@@ -23,7 +23,6 @@ class DocumentsController extends \yii\web\Controller
     {
         $emp = UserHelper::GetEmployee();
         $searchModel = new DocumentSearch([
-            'thai_year' => (date('Y') + 543),
             'date_filter' => 'today'
         ]);
         $dataProvider = $searchModel->search($this->request->queryParams);
@@ -55,16 +54,6 @@ class DocumentsController extends \yii\web\Controller
         }
         $dataProvider->query->andFilterWhere(['d_tags.to_id' => $emp->id]);
 
-        if ($searchModel->date_filter) {
-            $range = DateFilterHelper::getRange($searchModel->date_filter);
-            $searchModel->date_start = AppHelper::convertToThai($range[0]);
-            $searchModel->date_end = AppHelper::convertToThai($range[1]);
-        }
-
-        if ($searchModel->date_filter == '' && $searchModel->thai_year !== '' && $searchModel->thai_year !== null) {
-            $searchModel->date_start = AppHelper::convertToThai(($searchModel->thai_year - 544) . '-10-01');
-            $searchModel->date_end = AppHelper::convertToThai(($searchModel->thai_year - 543) . '-09-30');
-        }
 
         $dataProvider->setSort(['defaultOrder' => [
             // 'doc_regis_number' => SORT_DESC,
@@ -86,7 +75,6 @@ class DocumentsController extends \yii\web\Controller
         $department = $emp->department;
 
         $searchModel = new DocumentSearch([
-            'thai_year' => (date('Y') + 543),
             'date_filter' => 'today'
         ]);
 
@@ -118,17 +106,6 @@ class DocumentsController extends \yii\web\Controller
         }
 
         $dataProvider->query->andWhere(['d_department.to_id' => $emp->department]);
-
-       if($searchModel->date_filter) {
-            $range = DateFilterHelper::getRange($searchModel->date_filter);
-            $searchModel->date_start = AppHelper::convertToThai($range[0]);
-            $searchModel->date_end = AppHelper::convertToThai($range[1]);
-        }
-
-        if ($searchModel->date_filter == '' && $searchModel->thai_year !== '' && $searchModel->thai_year !== null) {
-            $searchModel->date_start = AppHelper::convertToThai(($searchModel->thai_year - 544) . '-10-01');
-            $searchModel->date_end = AppHelper::convertToThai(($searchModel->thai_year - 543) . '-09-30');
-        }
 
 
         return $this->render('index', [
