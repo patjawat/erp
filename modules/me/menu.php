35708361fc4cea8f0819dee539eda005f1f7f18b
patjawat <patjawat@gmail.com>
2026-01-06 20:49:11 +0700

    update menu

--- modules/me/menu.php
+++ modules/me/menu.php
@@ -26,7 +26,7 @@ $menus = [
         ภาพรวม
     </a>
     <?php if (!Yii::$app->user->can('branch')): ?>
-        <a href="<?= Url::to(['/approve']) ?>" class="btn <?= $active !== 'approve' ? 'btn-outline-primary' : 'btn-primary' ?>">
+        <a href="<?= Url::to(['/approve-v2']) ?>" class="btn <?= $active !== 'approve' ? 'btn-outline-primary' : 'btn-primary' ?>">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round-check-icon lucide-user-round-check">
                 <path d="M2 21a8 8 0 0 1 13.292-6" />
                 <circle cx="10" cy="8" r="5" />
