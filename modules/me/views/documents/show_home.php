3cfb0e85d5fb5d9e46dfc442b9155ed72e52ed9c
patjawat <patjawat@gmail.com>
2026-01-16 12:00:19 +0700

    แก้การแสดงผล responseceive document

--- modules/me/views/documents/show_home.php
+++ modules/me/views/documents/show_home.php
@@ -12,101 +12,102 @@ $me = UserHelper::GetEmployee();
 
 
 ?>
+
 <?php Pjax::begin(['id' => 'document-container', 'enablePushState' => false, 'timeout' => 5000]);?>
 <div class="card">
-    <div class="card-body">
-        <div class="d-flex justify-content-between align-top align-items-center">
-            <h6>
+    <div class="card-body p-2 p-md-3"> <div class="d-flex flex-column flex-sm-row justify-content-between align-items-sm-center mb-3">
+            <h6 class="mb-2 mb-sm-0">
                 <i class="bi bi-ui-checks"></i> ทะเบียนหนังสือ
-                <span
-                    class="badge rounded-pill text-bg-primary"><?php echo number_format(($dataProvider->getTotalCount()), 0) ?></span>
+                <span class="badge rounded-pill text-bg-primary"><?php echo number_format(($dataProvider->getTotalCount()), 0) ?></span>
                 รายการ
             </h6>
-            <?=Html::a('แสดงทั้งหมด',['/me/documents'],['class' => 'btn btn-sm btn-light rounded-pill','data' => ['pjax' => 0]])?>
+            <?=Html::a('แสดงทั้งหมด',['/me/documents'],['class' => 'btn btn-sm btn-light rounded-pill w-auto','data' => ['pjax' => 0]])?>
         </div>
-        <table class="table table-striped table-fixed">
-            <thead>
-                <tr>
-                    <th class="text-center" style="width:50px;">ลำดับ</th>
-                    <th class="text-center" style="min-width:100px; width:100px;">เลขที่รับ</th>
-                    <th style="min-width:320px;">เรื่อง</th>
-                    <th style="min-width:250px;">ผู้บันทึก</th>
-                    <th style="min-width:100px;">สถานะ</th>
-                    <th style="width:70px;">ดำเนินการ</th>
-                </tr>
-            </thead>
-            <tbody class="align-middle  table-group-divider table-hover">
-                <?php foreach($dataProvider->getModels() as $key => $item):?>
-                <tr id="<?=$item->id?>">
-                    <td class="text-center"><?php echo (($dataProvider->pagination->offset + 1)+$key)?></td>
-                    <td class="text-center">
-                        <?= isset($item->document) ? $item->document->doc_regis_number : ''?></td>
-                    <td class="fw-light align-middle">
-                        <div>
-                            <h6 style="width:600px" class="text-truncate mb-0">
-                                <?php if(isset($item->document) &&  $item->document->doc_speed == 'ด่วนที่สุด'):?>
-                                <span class="badge text-bg-danger fs-13">
-                                    <i class="fa-solid fa-circle-exclamation"></i> ด่วนที่สุด
-                                </span>
+
+        <div class="table-responsive">
+            <table class="table table-striped align-middle">
+                <thead>
+                    <tr class="text-nowrap"> <th class="text-center" style="width:50px;">ลำดับ</th>
+                        <th class="text-center" style="width:100px;">เลขที่รับ</th>
+                        <th style="min-width:300px;">เรื่อง</th>
+                        <th style="min-width:180px;">ผู้บันทึก</th>
+                        <th style="width:100px;">สถานะ</th>
+                        <th class="text-center" style="width:70px;">จัดการ</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    <?php foreach($dataProvider->getModels() as $key => $item):?>
+                    <tr id="<?=$item->id?>">
+                        <td class="text-center"><?php echo (($dataProvider->pagination->offset + 1)+$key)?></td>
+                        <td class="text-center fw-bold">
+                            <?= isset($item->document) ? $item->document->doc_regis_number : ''?>
+                        </td>
+                        <td>
+                            <div class="d-flex flex-wrap gap-1 mb-1"> <?php if(isset($item->document) &&  $item->document->doc_speed == 'ด่วนที่สุด'):?>
+                                    <span class="badge text-bg-danger fs-12"><i class="fa-solid fa-circle-exclamation"></i> ด่วนที่สุด</span>
                                 <?php endif;?>
 
                                 <?php if(isset($item->document) && $item->document->secret == 'ลับที่สุด'):?>
-                                <span class="badge text-bg-danger fs-13"><i class="fa-solid fa-lock"></i>
-                                    ลับที่สุด
-                                </span>
+                                    <span class="badge text-bg-danger fs-12"><i class="fa-solid fa-lock"></i> ลับที่สุด</span>
                                 <?php endif;?>
-                                <a href="<?php echo Url::to(['/me/documents/view','id' => $item->id,'callback' => '/me'])?>"
-                                    class="open-modal view-document" data-size="modal-xxl" data-tr-id="<?=$item->id?>">
-                                    เรื่อง : <?php echo $item->document ? $item->document->topic : ''?>
-                                </a>
-                                <?php echo  $item->document ? ($item->document->isFile() ? '<i class="fas fa-paperclip"></i>' : '') : ''?>
-                            </h6>
-                        </div>
-                        <p class="fw-normal fs-13 mb-0">
-                            <?= $item->document ? $item->data_json['des'] ?? '' : ''?>
-                        </p>
-                        <?php // echo Html::img('@web/img/krut.png',['style' => 'width:20px']);?>
-                        <span class="text-danger">
-                            <?php echo  $item->document ? $item->document->doc_number : ''?>
-                        </span>
-                        <span class="text-primary fw-normal fs-13">
-                            |
-                            <i class="fa-solid fa-inbox"></i>
-                            <?php  echo  $item->document ? $item->documentOrg->title ?? '-' : '';?>
-                            <span class="badge rounded-pill badge-soft-secondary text-primary fw-lighter fs-13">
-                                <i class="fa-regular fa-eye"></i>
-                                <?php echo  $item->document ? $item->document->viewCount() : ''?>
+                            </div>
+
+                            <a href="<?php echo Url::to(['/me/documents/view','id' => $item->id,'callback' => '/me'])?>"
+                                class="open-modal view-document fw-bold text-decoration-none" 
+                                data-size="modal-xxl" 
+                                data-tr-id="<?=$item->id?>"
+                                style="display: block; max-width: 400px; overflow: hidden; text-overflow: ellipsis; white-space: normal;">
+                                เรื่อง : <?php echo $item->document ? $item->document->topic : ''?>
+                                <?php echo  $item->document ? ($item->document->isFile() ? ' <i class="fas fa-paperclip text-muted"></i>' : '') : ''?>
+                            </a>
+                            
+                            <p class="text-muted small mb-1">
+                                <?= $item->document ? $item->data_json['des'] ?? '' : ''?>
+                            </p>
+
+                            <div class="fs-12">
+                                <span class="text-danger border-end pe-2 mr-2">
+                                    <?= $item->document ? $item->document->doc_number : ''?>
+                                </span>
+                                <span class="text-primary ps-1">
+                                    <i class="fa-solid fa-inbox"></i>
+                                    <?php echo $item->document ? $item->documentOrg->title ?? '-' : '';?>
+                                    <span class="badge rounded-pill bg-light text-dark ms-1">
+                                        <i class="fa-regular fa-eye"></i>
+                                        <?php echo $item->document ? $item->document->viewCount() : ''?>
+                                    </span>
+                                </span>
+                            </div>
+                            <?php echo $item->document ? $item->document->StackDocumentTags('comment') : ''?>
+                        </td>
+                        <td>
+                            <div class="small text-nowrap">
+                                <?= $item->document ? $item->document->viewCreate()['avatar'] : '';?>
+                            </div>
+                        </td>
+                        <td>
+                            <span class="badge bg-info-subtle text-info">
+                                <?=$item->document->documentStatus->title ?? '-'?>
                             </span>
-                        </span>
+                        </td>
+                        <td class="text-center">
+                            <?php echo $item->document ? Html::a('<i class="fa-regular fa-pen-to-square fa-lg"></i>',['view', 'id' => $item->id,'callback' => '/me'],['class' => 'btn btn-outline-primary btn-sm open-modal view-document','data' => ['size' => 'modal-xxl']]) : ''?>
+                        </td>
+                    </tr>
+                    <?php endforeach;?>
+                </tbody>
+            </table>
+        </div>
 
-
-                        <?php echo  $item->document ? $item->document->StackDocumentTags('comment') : ''?>
-                    </td>
-                    <td class="fw-light align-middle">
-                        <div class=" d-flex flex-column">
-                            <?= $item->document ? $item->document->viewCreate()['avatar'] : '';?>
-                            <!-- <span class="fw-normal fs-6"><?php echo  $item->document ? $item->document->viewReceiveDate() : ''?></span>
-                            <span class="fw-lighter fs-13"><?php echo  $item->document ? (isset($item->document->doc_time) ? '<i class="fa-solid fa-clock"></i> '.$item->document->doc_time : '') :''?></span> -->
-                        </div>
-                    </td>
-                    <td> <?=$item->document->documentStatus->title ?? '-'?></td>
-                    <td><?php echo  $item->document ?  Html::a('<i class="fa-regular fa-pen-to-square fa-2x"></i>',['view', 'id' => $item->id,'callback' => '/me'],['class' => 'open-modal view-document','data' => ['size' => 'modal-xxl']]) : ''?>
-                    </td>
-                </tr>
-                <?php endforeach;?>
-
-            </tbody>
-        </table>
-        <div class="iq-card-footer text-muted d-flex justify-content-center mt-4">
-            <?= yii\bootstrap5\LinkPager::widget([
-                'pagination' => $dataProvider->pagination,
-                'firstPageLabel' => 'หน้าแรก',
-                'lastPageLabel' => 'หน้าสุดท้าย',
-                'options' => [
-                    'listOptions' => 'pagination pagination-sm',
-                    'class' => 'pagination-sm',
-                ],
-            ]); ?>
+        <div class="mt-4 d-flex justify-content-center">
+            <nav aria-label="Page navigation">
+                <?= yii\bootstrap5\LinkPager::widget([
+                    'pagination' => $dataProvider->pagination,
+                    'firstPageLabel' => '«',
+                    'lastPageLabel' => '»',
+                    'options' => ['class' => 'pagination pagination-sm flex-wrap justify-content-center'],
+                ]); ?>
+            </nav>
         </div>
     </div>
 </div>
