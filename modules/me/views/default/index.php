7c9c3205ebe7d302a4b59f144825566d4156d05d
patjawat <patjawat@gmail.com>
2026-01-22 00:30:32 +0700

    update

--- modules/me/views/default/index.php
+++ modules/me/views/default/index.php
@@ -80,8 +80,9 @@ $this->params['breadcrumbs'][] = ['label' => $me->fullname(), 'url' => ['/me']];
                                     d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z">
                                 </path>
                             </svg>
-                            <span class="text-white fw-black text-uppercase"
-                                style="font-size: 10px; letter-spacing: 0.05em;">ดาวเด่นโรงพยาบาล</span>
+                            <!-- <span class="text-white fw-black text-uppercase"
+                                style="font-size: 10px; letter-spacing: 0.05em;">ดาวเด่นโรงพยาบาล
+                            </span> -->
                         </div>
                     </div>
                     <p class="text-white text-opacity-75 text-sm fw-medium mb-4"><?= $me->positionName() ?> • <span
