3e9a68cad64df3b973f9a88e9fd9fd53ef06a8e8
patjawat <patjawat@gmail.com>
2026-01-21 23:32:34 +0700

    update

--- modules/me/menu.php
+++ modules/me/menu.php
@@ -35,27 +35,7 @@ $menus = [
             รายการที่รออนุมัติ <span class="badge rounded-pill badge-soft-primary text-primary ms-1"> <?= $total ?> </span>
         </a>
 
-        <div class="dropdown">
-            <button class="btn <?= $active !== 'setting' ? 'btn-outline-primary' : 'btn-primary' ?> dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-settings-icon lucide-settings">
-                    <path d="M9.671 4.136a2.34 2.34 0 0 1 4.659 0 2.34 2.34 0 0 0 3.319 1.915 2.34 2.34 0 0 1 2.33 4.033 2.34 2.34 0 0 0 0 3.831 2.34 2.34 0 0 1-2.33 4.033 2.34 2.34 0 0 0-3.319 1.915 2.34 2.34 0 0 1-4.659 0 2.34 2.34 0 0 0-3.32-1.915 2.34 2.34 0 0 1-2.33-4.033 2.34 2.34 0 0 0 0-3.831A2.34 2.34 0 0 1 6.35 6.051a2.34 2.34 0 0 0 3.319-1.915" />
-                    <circle cx="12" cy="12" r="3" />
-                </svg>
-                <span class="d-none d-sm-inline">เมนูบริการ</span>
-            </button>
 
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                <?php foreach ($menus as $menu): ?>
-                    <li>
-                        <?= Html::a(
-                            '<i class="' . $menu['icon'] . ' me-2"></i> ' . $menu['label'],
-                            $menu['url'],
-                            ['class' => 'dropdown-item']
-                        ) ?>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
     <?php endif; ?>
 
     <a href="<?= Url::to(['/me/store-v2/dashboard']) ?>" class="btn btn-outline-primary">
