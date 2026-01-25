5ef5e3f635f3fa3b12baa6ea235e70037812f076
patjawat <patjawat@gmail.com>
2026-01-25 21:51:34 +0700

    add migrate

---
+++ migrations/m260125_144943_add_send_org.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+class m260125_144943_add_send_org extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%documents}}', 'send_org', $this->integer()->after('data_json'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+
+        return true;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m260125_144943_add_send_org cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
