85269ff3397a48aacee4294e3a691af1d2858d0e
patjawat <patjawat@gmail.com>
2026-08-27 07:14:33 +0700

    chore: update dashboard labels and impeccable skills

--- modules/me/menu.php
+++ modules/me/menu.php
@@ -111,7 +111,7 @@ $menus = [
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-table-2">
                 <path d="M9 3H5a2 2 0 0 0-2 2v4m6-6h10a2 2 0 0 1 2 2v4M9 3v18m0 0h10a2 2 0 0 0 2-2V9M9 21H5a2 2 0 0 1-2-2V9m0 0h18" />
             </svg>
-            จัดตารางเวร
+            ตารางเวร
         </a>
         <a href="<?= Url::to(['/me/plan']) ?>" class="btn <?= $active !== 'plan' ? 'btn-outline-primary' : 'btn-primary' ?>">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clipboard-list">
@@ -119,7 +119,7 @@ $menus = [
                 <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
                 <path d="M12 11h4" /><path d="M12 16h4" /><path d="M8 11h.01" /><path d="M8 16h.01" />
             </svg>
-            จัดทำแผนหน่วยงาน
+            แผนหน่วยงาน
         </a>
     <?php endif; ?>
 
